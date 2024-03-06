d61a3353ebede7b1151b9053f77cdcf6879de25c
kmeeth <matijakup@gmail.com>
2024-03-06 19:48:54 +0100

    Implemented a tree-based integer partitions generator (buggy).

--- src/generators/IntegerPartitionsGeneratorFactory.cpp
+++ src/generators/IntegerPartitionsGeneratorFactory.cpp
@@ -1,12 +1,15 @@
 #include "h/generators/IntegerPartitionsGeneratorFactory.h"
 #include "h/generators/SimpleBacktrackingIntegerPartitionsGenerator.h"
+#include "h/generators/TreeIntegerPartitionsGenerator.h"
 
-const std::vector<std::string> IntegerPartitionsGeneratorFactory::algorithms = {"SimpleBacktracking"};
+const std::vector<std::string> IntegerPartitionsGeneratorFactory::algorithms = {"SimpleBacktracking", "Tree"};
 
 std::unique_ptr<IntegerPartitionsGenerator> IntegerPartitionsGeneratorFactory::make(const std::string_view name)
 {
     if(name == "SimpleBacktracking")
         return std::make_unique<SimpleBacktrackingIntegerPartitionsGenerator>();
+    else if(name == "Tree")
+        return std::make_unique<TreeIntegerPartitionsGenerator>();
     else
         return nullptr;
 }
