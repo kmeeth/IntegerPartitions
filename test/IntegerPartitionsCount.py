3f3e06ed99942a93a51357aee449bcab59e49d8b
kmeeth <matijakup@gmail.com>
2024-03-29 17:51:06 +0100

    Fixed the counting script for integers.

--- test/IntegerPartitionsCount.py
+++ test/IntegerPartitionsCount.py
@@ -12,22 +12,24 @@ def partition(n, k):
         return 1
     if n <= 0 or k <= 0:
         return 0
-    return partition(n - k, k) + partition(n, k - 1)
+    return partition(n - k, k) + partition(n - 1, k - 1)
 
 
 def main():
-    N = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 3000, 4000]
-    K = [3, 4, 5, 6, 7, 8, 9, 10, 0.5]
+    N = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000]
+    K = [3, 4, 5, 6, 7, 8, 9, 10, -2, -3, -4, 0.5]
+
     for k in K:
-        row = []
         for n in N:
-            k_actual = k
-            if k_actual < 0:
-                k_actual += n
-            elif k_actual == 0.5:
-                k_actual = n // 2
-            row.append(partition(n, k_actual))
-        print(row)
+            with open("out.txt", mode="a") as file:
+                k_actual = k
+                if k_actual < 0:
+                    k_actual += n
+                elif k_actual == 0.5:
+                    k_actual = n // 2
+                file.write(f"{partition(n, k_actual)} ")
+        with open("out.txt", mode="a") as file:
+            file.write("\n")
 
 
 if __name__ == "__main__":
