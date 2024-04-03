48b4f404b590c08a5cd31a29c6a209ce3d0feec6
kmeeth <matijakup@gmail.com>
2024-04-04 01:42:08 +0200

    Poor attempts so far.

--- test/script/AutoBenchmark.ps1
+++ test/script/AutoBenchmark.ps1
@@ -1,10 +1,10 @@
 param (
-    [int[]]$N_int = @(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 600000, 70000, 80000, 90000, 100000),
-    [int[]]$K_int = @(3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60, 75, 100, 150, 200, 250, 300, -2 , -3, -4, 0),
-    [int[]]$N_set = @(14, 16, 18, 20, 22, 24, 26, 28, 30, 35, 40, 50, 60),
-    [int[]]$K_set = @(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, -1, -2, -3, 0.5),
+    [int[]]$N_int = @(100),#, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 600000, 70000, 80000, 90000, 100000),
+    [int[]]$K_int = @(3),#, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60, 75, 100, 150, 200, 250, 300, -2 , -3, -4, 0),
+    [int[]]$N_set = @(14),#, 16, 18, 20, 22, 24, 26, 28, 30, 35, 40, 50, 60),
+    [int[]]$K_set = @(2),#, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, -1, -2, -3, 0.5),
     [System.Object[]]$algs = @(@("int", "Tree"), @("int", "Conjugation"), @("int", "SimpleBacktracking"), @("set", "Filter"), @("set", "SimpleBacktracking")),
-    [int]$maxProcesses = 1
+    [int]$maxProcesses = 2
 )
 
 # List to keep track of running jobs
@@ -17,17 +17,21 @@ function Start-NewJob {
         [string]$appArguments
     )
     $argumentList = "-appArguments `"$appArguments`""
-
-    $job = Start-Process "AppTimedWrapper.ps1" -ArgumentList $argumentList -PassThru -ErrorAction Stop
-    $jobs += $job
+    Write-Output $argumentList
+    $scriptBlock = {
+        param($arguments)
+        [console]::Beep()
+        #& "AppTimedWrapper.ps1" $args
+    }
+    $job = Start-Job -ScriptBlock $scriptBlock -ArgumentList $argumentList
+    $global:jobs += $job
 }
 
 # Check if any jobs have completed and remove them from the list
 function Check-JobCompletion {
-    $completedJobs = $jobs | Where-Object { $_.HasExited }
+    $completedJobs = $jobs | Where-Object { $_.Finished }
     foreach ($completedJob in $completedJobs) {
-        $jobs.Remove($completedJob) | Out-Null
-        $completedJob | Remove-Job
+        $jobs.Remove($completedJob)
     }
 }
 
@@ -47,10 +51,10 @@ function Iterate
     if($k -gt $n) {
         return
     }
-    Write-Output $n $k
 
     # Check if the maximum number of processes is reached
     while ($jobs.Count -ge $maxProcesses) {
+        Write-Output $jobs.Count
         Start-Sleep -Seconds 5
         Check-JobCompletion
     }
@@ -62,8 +66,7 @@ function Iterate
     # Start a new job with your program and arguments
     $program = "AppTimedWrapper.ps1"
     $arguments = ("-mode " + $mode + " -alg " + $alg + " -visit Counter -rout " + $rout + " -n " + $n + " -k " + $k)
-    Start-NewJob -program $program -arguments $arguments
-
+    Start-NewJob -program $program -appArguments $arguments
 }
 
 # Main loop to manage starting new jobs
@@ -82,5 +85,10 @@ foreach($a in $algs) {
             }
         }
     }
-
+}
+# Join all
+while ($jobs.Count -gt 0) {
+    Write-Output $jobs.Count
+    Start-Sleep -Seconds 5
+    Check-JobCompletion
 }
