e7acde04c373e45fd9add53d67372377a6f7c9eb
kmeeth <matijakup@gmail.com>
2024-04-08 01:44:55 +0200

    Fixed a bug in AppTimedWrapper.ps1.

--- test/script/AppTimedWrapper.ps1
+++ test/script/AppTimedWrapper.ps1
@@ -1,10 +1,11 @@
 param (
-    [string]$appArguments = "-mode int -alg Tree -visit Counter -rout out.txt -n 10000 -k 3000",
+    [string]$appArguments = "-mode int -alg Tree -visit Counter -rout out.txt -n 1 -k 1",
     [int]$timeoutInMs = 3600000 # Default timeout is 1 hour
 )
 
 try {
     Write-Output $appArguments
+    Write-Output $timeoutInMs
     $argumentArray = $appArguments -split '\s+'
     $processStartInfo = New-Object System.Diagnostics.ProcessStartInfo
     $processStartInfo.FileName = "../../bin/PartitionsGeneration.exe"
@@ -13,9 +14,9 @@ try {
     $processStartInfo.UseShellExecute = $false
     $processStartInfo.WorkingDirectory = "../../bin"
     $process = [System.Diagnostics.Process]::Start($processStartInfo)
-    $output = $process.StandardOutput.ReadToEnd()
-    # Wait for the process to finish or timeout
+    #Wait for the process to finish or timeout
     if ($process.WaitForExit($timeoutInMs)) {
+        $output = $process.StandardOutput.ReadToEnd()
         Write-Host ($appArguments + " completed with exit code $($process.ExitCode)")
     } else {
         Write-Host ($appArguments + " timed out. Killing...")
