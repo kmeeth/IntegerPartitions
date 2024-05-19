7c6951051b6abecd821e97f6f5a9e76b030024ca
kmeeth <matijakup@gmail.com>
2024-05-19 18:56:21 +0200

    Improved integer partitions counting.

--- test/script/IntegerPartitionsCount.py
+++ test/script/IntegerPartitionsCount.py
@@ -3,7 +3,7 @@ import threading
 from functools import cache
 
 threading.stack_size(4096 * 10000)
-sys.setrecursionlimit(2**31-1)
+sys.setrecursionlimit(1<<31-1)
 
 
 @cache
@@ -18,19 +18,28 @@ def partition(n, k):
 
 
 def main():
-    N = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000]
-    K = [3, 4, 5, 6, 7, 8, 9, 10, -2, -3, -4, 0.5]
+    N = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 600000, 70000, 80000, 90000, 100000]
+    K = [3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60, 75, 100, 150, 200, 250, 300, -2 , -3, -4]
 
+    with open("../results/partitionCount.int", mode="a+") as file:
+        file.write(f" ,")
     for k in K:
-        for n in N:
-            with open("../output/partitionCount.int", mode="a") as file:
-                k_actual = k
-                if k_actual < 0:
-                    k_actual += n
-                elif k_actual == 0.5:
-                    k_actual = n // 2
-                file.write(f"{partition(n, k_actual)} ")
-        with open("../output/partitionCount.int", mode="a") as file:
+        with open("../results/partitionCount.int", mode="a+") as file:
+            file.write(f"{k},")
+    with open("../results/partitionCount.int", mode="a+") as file:
+        file.write("\n")
+    for n in N:
+        with open("../results/partitionCount.int", mode="a+") as file:
+            file.write(f"{n},")
+        for k in K:
+            k_actual = k
+            if k_actual < 0:
+                k_actual += n
+            elif k_actual == 0.5:
+                k_actual = n // 2
+            with open("../results/partitionCount.int", mode="a+") as file:
+                file.write(f"{partition(n, k_actual)},")
+        with open("../results/partitionCount.int", mode="a+") as file:
             file.write("\n")
 
 
